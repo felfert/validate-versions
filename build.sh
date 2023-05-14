7ad5ecf84f00d3fad2be8e8b3fd009351cc97d12
Fritz Elfert <fritz@fritz-elfert.de>
2023-05-14 13:12:34 +0200

    Remove dist

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+test -f package.json || npm init -y
+npm install @actions/core
+npm install @actions/github
+npm install @iarna/toml
+npm install @vercel/ncc
+./node_modules/.bin/ncc build index.js -o dist
+cp action.yml dist/
